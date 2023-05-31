11ff7e697937a5e54fc156aef86b156ad2278431
Renan Menezes <2003rcsm@gmail.com>
2023-05-31 16:48:11 -0300

    Adaptando a Urna do javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+/*
+console.log("Mensagem BRAZIL");
+
+console.clear();
+
+console.log("@-@ Soma de dois números\n");
+
+let primeiroNumero = parseInt(prompt("Digite p primeiro número:"));
+let segundoNumero = parseInt(prompt("Digite o segundo número:"));
+
+let soma = parseInt(primeiroNumero) + parseInt(segundoNumero);
+
+console.log("Soma dos números: ", soma);*/
+
+/*URNA ELETRONICA*/
+
+let codigoVoto,
+    votosTotais = 0,     
+    votosCandidato1 = 0,
+    votosCandidato2 = 0,
+    votosCandidato3 = 0,
+    votosBranco = 0,
+    votosNulos = 0,
+    votosTotaisGanhador = 0
+
+let 
+    percentualVotosCandidato1 = 0.0, 
+	percentualVotosCandidato2 = 0.0,
+	percentualVotosCandidato3 = 0.0,
+	percentualVotosBranco = 0.0,
+	percentualVotosNulo = 0.0,
+	percentualGanhador = 0.0
+
+let 
+    nomeCandidato1 = "Celso Portioli",
+    nomeCandidato2 = "Luciano Huck",
+    nomeCandidato3 = "Pedro de Lara",
+    nomeGanhador = ""
+
+    
+    
+    opcaoInvalida = false;
